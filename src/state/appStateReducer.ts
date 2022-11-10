fd22d1ba6880888db3f04a0ae3cdcca6794bfc93
Jorge Machuca <jmachuca@bond.com.py>
2022-11-10 12:52:27 -0300

    added state reducer

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -4,3 +4,21 @@ export type Task = {
   id: string;
   text: string;
 };
+
+export type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+};
+
+export const appStateReducer = (state: AppState, action: Action): AppState => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
